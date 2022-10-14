15daa27ba9ca96b2a356157f27297f1322b39e38
ksjwjsisjjjs <112697453+ksjwjsisjjjs@users.noreply.github.com>
2022-10-14 23:15:04 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,5 +1,5 @@
 pkg update -y
-pkg install php php7 -y
+pkg install curl php7 -y
 wget https://github.com/ksjwjsisjjjs/tembak-tri/raw/main/tembak.php
 echo -e "\n\n\n=========\n\njalankan perintah dibawah ini:\n\nphp tembak.php\n\n==========\n\n"
 rm -f gas.sh
