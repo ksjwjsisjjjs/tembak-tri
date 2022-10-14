3aeb891075bda8bbf0b44b9a93752d4eb4a6f1db
ksjwjsisjjjs <112697453+ksjwjsisjjjs@users.noreply.github.com>
2022-10-14 23:03:16 +0700

    Create gas.sh

---
+++ gas.sh
@@ -0,0 +1,5 @@
+pkg update -y
+pkg install php php7 -y
+wget https://github.com/ksjwjsisjjjs/tembak-tri/raw/main/tembak.php
+echo -e "\n\n\n=========\n\njalankan perintah dibawah ini:\n\nphp tembak.php\n\n==========\n\n"
+rm -f start.sh
